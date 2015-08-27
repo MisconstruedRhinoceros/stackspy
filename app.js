91bc4338c84d65ebd62db729aec462faa7c0adeb
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-27 16:20:10 -0700

    Return object if required, console log otherwise

--- app.js
+++ app.js
@@ -5,13 +5,35 @@ var clientID = require('./client');
 var main = function(url) {
 
   console.log("Spying on", url, "...");
-  var client_results = [];
-  var server_results = [];
 
   request(url, function(err, response, body) {
     if(!err) {
-      server_results.push(serverID(response)); 
-      client_results.push(clientID(body));
+      var server_results = serverID(response); 
+      var client_results = clientID(body);
+
+      /** Directly run has process.argv */
+      if(process.argv) {
+        console.log('\n');
+        console.log("-----RESULTS-----");
+       
+        console.log("CLIENT SIDE: ");
+        if(client_results.length) {
+          console.log(client_results);
+        }
+        
+        console.log("SERVER SIDE: ");
+        if(server_results.length) {
+          console.log(server_results);
+        }
+      
+      /** No argv means we are imported */
+      } else {
+        return {
+          server: server_results,
+          client: client_results
+        }
+      }
+
     } else {
       console.log(err);
     }
