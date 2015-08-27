1f34d80aec14d3ef77e758362b123c15bf798198
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-27 15:07:40 -0700

    Pass url into main function in app.js

--- app.js
+++ app.js
@@ -2,40 +2,22 @@ var request = require('request');
 var serverID = require('./server');
 var clientID = require('./client');
 
-var testSites = ['https://www.airbnb.com',
-                 'https://asana.com',
-                 'https://www.atlassian.com',
-                 'https://www.bbc.com',
-                 'https://www.cloudflare.com',
-                 'https://www.facebook.com',
-                 'https://imgur.com',
-                 'https://instagram.com',
-                 'https://www.khanacademy.org',
-                 'https://www.kissmetrics.com',
-                 'https://www.netflix.com',
-                 'https://www.pivotaltracker.com',
-                 'https://www.quizup.com/en',
-                 'https://www.reddit.com',
-                 'https://www.squarespace.com'
-                ];
 var main = function(url) {
 
   var client_results = [];
   var server_results = [];
 
-  for(var i = 0; i < testSites.length; i++) {
-    request(testSites[i], function(err, response, body) {
-      if(!err) {
-        server_results.push(serverID(response)); 
-        client_results.push(clientID(body));
-      } else {
-        console.log(err);
-      }
-    });
-  }
+  request(url, function(err, response, body) {
+    if(!err) {
+      server_results.push(serverID(response)); 
+      client_results.push(clientID(body));
+    } else {
+      console.log(err);
+    }
+  });
 }
 
 if(require.main === module) {
-  main();
+  main(process.argv[2]);
 }
 module.exports = main;
