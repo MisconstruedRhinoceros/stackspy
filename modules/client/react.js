97284f46d1ab6445fc1e9656165119ddba904986
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-08-27 15:56:44 -0700

    Return results in jquery module

--- modules/client/react.js
+++ modules/client/react.js
@@ -13,7 +13,13 @@ module.exports = function(response) {
   var $scripts = $('script');
 
   for(var i = 0; i < $scripts.length; i++) {
+    
     if($scripts[i].attribs.src) {
+
+      /**
+       * Tilde is a quick way to transform -1
+       * to false
+       */
       if(~$scripts[i].attribs.src.indexOf('react')) {
         result.react = true;
         console.log('React: ', $scripts[i].attribs.src);
@@ -22,4 +28,6 @@ module.exports = function(response) {
       
     }
   }
+
+  return result;
 }
